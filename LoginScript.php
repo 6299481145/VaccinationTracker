06e5b24b55f368239d5c9fb9805b9b0656889006
Neha Bharti <nb873809@gmail.com>
2022-03-21 19:17:49 +0530

    Use prepare statement in login form.

--- LoginScript.php
+++ LoginScript.php
@@ -4,14 +4,17 @@ $password = $_POST['password'];
 
 include 'Connect.php';
 
-$sql = "SELECT * FROM user WHERE user_id='$user_id' AND password='$password'";
-
-$result = $con->query($sql);
-
+//$sql = "SELECT * FROM user WHERE user_id='$user_id' AND password='$password'";
+$sql = "SELECT * FROM user WHERE user_id = ? AND password = ?";
+//$result = $con->query($sql);
+$stmt = $con->prepare($sql); 
+$stmt->bind_param("ss", $user_id,$password);
+$stmt->execute();
+$result = $stmt->get_result();
 if($result->num_rows>0) {
     echo "<script>window.location.href='Home.php'</script>";
 } else {
     echo "Login failed!";
 }
-
+$stmt->close();
 $con->close();
